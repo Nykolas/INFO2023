0363a07643b4c593ce9944ffb903d5fad25b16e9
Nicolas <nicotortosa@gmail.com>
2023-07-13 10:48:34 -0300

    creamos modelos

--- RepositorioProyecto/proyectoFinal/proyectoFinal/settings/base.py
+++ RepositorioProyecto/proyectoFinal/proyectoFinal/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.noticias',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ STATICFILES_DIRS = (
     )
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- RepositorioProyecto/proyectoFinal/proyectoFinal/urls.py
+++ RepositorioProyecto/proyectoFinal/proyectoFinal/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth
+
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name = 'home'),
@@ -28,4 +31,4 @@ urlpatterns = [
     #URLS DE USUARIOS
     path('usuarios/', include('apps.usuarios.urls'))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
